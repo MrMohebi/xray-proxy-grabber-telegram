858691661a8dc6fcad322c1be0ffed591ca1ae97
MMMohebi <MMMohebi@outlook.com>
2023-12-31 16:14:41 +0330

    dont let grow all row urls

---
+++ dontLetGrowAllRowProxies.py
@@ -0,0 +1,14 @@
+import shutil
+from gitRepo import getLatestRowProxies, commitPushRowProxiesFile
+
+with open("collected-proxies/row-url/all.txt", 'r') as rowProxiesFile:
+    if len(rowProxiesFile.readlines()) < 1000:
+        print("row proxies count(under 1000) => ", rowProxiesFile)
+        exit(1)
+
+
+getLatestRowProxies()
+
+shutil.copyfile("collected-proxies/row-url/actives.txt", "collected-proxies/row-url/all.txt")
+
+commitPushRowProxiesFile("------cleaning all row url list base on actives-------")
